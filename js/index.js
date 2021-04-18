ef1334511bf3750e6e612d01fe7365a413100685
Manish Singh Rawat <53502278+Manish889@users.noreply.github.com>
2021-04-18 14:04:39 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,7 @@
+'use strict';
 
-// Toggle between DAy and Night Mode
-$("#switch1").on("click", function () {
-  $("body").toggleClass("night");
-});
+let date = new Date();
+document.getElementById('date').innerHTML = date.getHours() + ':' + date.getMinutes() + '/ ' + date.getDay() + '-' + date.getMonth() + '-' + date.getFullYear();
+document.getElementById('date').style.height = 'auto';
+document.getElementById('date').style.fontSize = '18px'
 
-// If user visit after 7 pm in night the body will change the style to night class
-$(document).ready(function () {
-  var date = new Date();
-  var current_time = date.getHours();
-  if (current_time > 19 || current_time < 6)
-    // If time is after 7PM or before 6AM, apply night theme to ‘body’
-    document.body.className = "night";
-  // Else use ‘day’ theme
-  else document.body.className = "day";
-});
-
-$(window).load(function () {
-  $("#top-button").hide();
-});
