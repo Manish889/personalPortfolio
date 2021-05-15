f4926d81a6938242050326b5d8e0cfc084bd1632
Manish Singh Rawat <53502278+Manish889@users.noreply.github.com>
2021-05-15 12:12:16 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,46 @@
-'use strict';
+"use strict";
 
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let date = new Date();
-document.getElementById('date').innerHTML = date.getHours() + ':' + date.getMinutes() + '/ ' + date.getDay() + '-' + date.getMonth() + '-' + date.getFullYear();
-document.getElementById('date').style.height = 'auto';
-document.getElementById('date').style.fontSize = '18px'
+document.getElementById("month").innerHTML = monthNames[date.getMonth()];
+document.getElementById("date").innerHTML = date.getFullYear();
 
+// Dark mode
+
+document.addEventListener("DOMContentLoaded", () => {
+  const themeStylesheet = document.getElementById("theme");
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) {
+    themeStylesheet.href = storedTheme;
+  }
+  const themeToggle = document.getElementById("theme-toggle");
+  const navbarText = document.getElementById("navbar");
+  themeToggle.addEventListener("click", () => {
+    // if it's light -> go dark
+    if (themeStylesheet.href.includes("Day")) {
+      themeStylesheet.href = "css/night-theme.css";
+      themeToggle.innerText = "Light mode";
+      navbarText.classList.toggle("navbar-dark");
+    } else {
+      // if it's dark -> go light
+      themeStylesheet.href = "css/Day-theme.css";
+      themeToggle.innerText = "Dark mode";
+      navbarText.classList.remove("navbar-dark");
+    }
+
+    localStorage.setItem("theme", themeStylesheet.href);
+  });
+});
